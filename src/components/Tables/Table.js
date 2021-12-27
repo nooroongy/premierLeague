01e61df26cab1b312ffa91b4f9fb7e758e89e20b
nooroongy <haerammy@gmail.com>
2021-12-28 00:32:34 +0900

    learn scss

--- src/components/Tables/Table.js
+++ src/components/Tables/Table.js
@@ -1,9 +1,12 @@
-
+import '../../css/table.css'
 
 const Table = ({ clubs }) => {
     return <div>
         Table component
-        {clubs.map((v, i) => <div key={i}>{v}</div>)}
+        {clubs.map((v, i) => 
+        <div
+            className={'test'+i%3}
+            key={i}>{v}</div>)}
     </div>
 }
 
